21bddacc4f1a5e3bf1e00a762277770d72e251e0
Bruce WL <78944540+Bruce-WL@users.noreply.github.com>
2022-01-02 11:20:39 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成崔佑嘉';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
